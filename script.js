5f4c8fb7b741197ce9ee39cd7555663f0837102e
whatisgonee <83175057+whatisgonee@users.noreply.github.com>
2021-09-28 21:28:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
 		if(isWhite) rabbitHole.style.color = "black";
 		else rabbitHole.style.color = "white";
 		setTimeout(() => {
-			if(width <= window.screen.availWidth) {
+			if(width <= window.screen.availWidth && window.screen.availWidth > window.screen.availHeight) {
 				width += window.screen.availWidth / 20;
+				rabbitHole.style.width = width + "px";
+				rabbitHole.style.height = width + "px";
+
+				zoom();
+			} else if(width <= window.screen.availHeight && window.screen.availHeight > window.screen.availWidth) {
+				width += window.screen.availHeight / 20;
 				rabbitHole.style.width = width + "px";
 				rabbitHole.style.height = width + "px";
 
@@ -44,6 +50,58 @@ document.addEventListener('DOMContentLoaded', () => {
 	function reset() {
 		var circleColor = "";
 		var mainColor = "";
+		if(isWhite) {
+			circleColor = "black";
+			mainColor = "white";
+		} else {
+			circleColor = "white";
+			mainColor = "black";
+		}
+		document.body.style.backgroundColor = mainColor;
+		rabbitHole.style.width = "0px";
+		rabbitHole.style.height = "0px";
+		rabbitHole.style.borderRadius = "50%";
+		rabbitHole.style.backgroundColor = "transparent";
+		rabbitHole.style.cursor = "pointer";
+		rabbitHole.innerHTML = "#" + count;
+		document.getElementById("main").style.cursor = "default";
+		setTimeout(() => {
+			rabbitHole.style.backgroundColor = circleColor;
+			resetZoom();
+		}, 500);
+	}
+
+	function resetZoom() {
+		setTimeout(() => {
+			if(width < 300) {
+				width += 20;
+				rabbitHole.style.width = width + "px";
+				rabbitHole.style.height = width + "px";
+				resetZoom();
+			} else {
+				rabbitHole.addEventListener('click', zoom);
+			}
+		}, 100);
+	}
+
+	function event() {
+		var replicas = text.match(/\/\/(.*): (.*)/g);
+		
+		for(var i = 0; i < replicas.length; i++) {
+			var divided = replicas[i].match(/\/\/(.*): (.*)/);
+			if(count == parseInt(divided[1])) message.innerHTML = divided[2]; 
+		}
+	}
+
+	function fetch() {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://raw.githubusercontent.com/whatisgonee/rabbitHole/main/replicas.txt", false);
+		xmlhttp.send();
+		if (xmlhttp.status==200) {
+			text = xmlhttp.responseText;
+		}
+	}
+});
 		if(isWhite) {
 			circleColor = "black";
 			mainColor = "white";
